149174a83eddfaf5e7a498bcbb8809148292dbca
tprdk <tprdk@outlook.com>
2023-01-05 14:40:47 +0300

    server init

---
+++ server/src/test/java/com/cc/caloriecalculator/CalorieCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.caloriecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
